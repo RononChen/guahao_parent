f91067b1e7337f073952acac0cf084c8815bbe76
chennikowrod <nikoword0411@gmail.com>
2022-09-11 14:29:32 +0800

    hospitalSet 的增删改查全部完成  MD5加密方式

--- service/service_hosp/src/main/java/com/wei/guahao/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/wei/guahao/controller/HospitalSetController.java
@@ -8,13 +8,16 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wei.guahao.service.HospitalSetService;
 import com.wei.yygh.common.result.Result;
+import com.wei.yygh.common.utils.MD5;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Time;
 import java.util.List;
+import java.util.Random;
 
 @Api(tags = "这是医院设置类")
 @RestController
@@ -73,6 +76,76 @@ public class HospitalSetController {
         return Result.ok(hospitalSetPage);
     }
 
+    @ApiOperation(value = "添加医院设置")
+//    4、添加医院设置   请求过来的时候封装成一个HospitalSet
+    @PostMapping("/saveHospitalSet")
+    public Result saveHospitalSet(@RequestBody HospitalSet hospitalSet){
+//        4.1、签名秘钥 和 状态
+        Random random = new Random();
+//        使用MD5进行加密处理  需要添加MD5工具类
+        String encrypt = MD5.encrypt(random.nextInt(100) + "" + System.currentTimeMillis());
+        hospitalSet.setSignKey(encrypt);
+//        设置状态 1 使用 0 不能使用
+        hospitalSet.setStatus(1);
+        hospitalSetService.save(hospitalSet);
+        return  Result.ok();
+    }
 
+//    5、根据id获取 医院设置
+    @ApiOperation(value = "根据id获取 医院设置")
+    @GetMapping("{id}")
+    public Result selectHospitalSetById(@PathVariable long id){
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        return Result.ok(hospitalSet);
+    }
+
+//    6、修改医院设置 也就是更新  需要传递进来一个封装好的HospitalSet
+    @ApiOperation(value = "修改医院设置")
+    @PostMapping("/updateHospitalSet")
+    public Result updateHospitalSet(@RequestBody HospitalSet hospitalSet){
+//        根据传递进来的hospitalSet进行更新
+        hospitalSetService.updateById(hospitalSet);
+        return Result.ok();
+    }
+
+//    7、批量删除 医院设置
+    @ApiOperation(value = "批量删除 医院设置")
+    @DeleteMapping("/benchRemoveHospitalSet")
+    public  Result benchRemoveHospitalSet(@RequestBody List<Long> list){
+        boolean b = hospitalSetService.removeByIds(list);
+        if (b){
+            return Result.ok();
+        }else {
+            return Result.fail();
+        }
+    }
+
+    /**
+     *  8、医院设置  锁定和解锁  status字段进行状态操作   1 使用 0 不能使用
+     *  先根据id来查询出来具体的医院  然后设置医院的status  最后更新这个医院设置
+     */
+    @ApiOperation(value = "医院设置  锁定和解锁")
+    @PutMapping("/changeHospitalSetStatus/{id}/{status}")
+    public Result changeHospitalSetStatus(@PathVariable Long id,
+                                          @PathVariable Integer status){
+//        8.1、根据id查询 医院设置
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+//        8.2、修改医院设置的状态
+        hospitalSet.setStatus(1);
+//        8.3、更新这个  医院设置
+        hospitalSetService.updateById(hospitalSet);
+        return Result.ok();
+    }
+
+    //9 发送签名秘钥  发送短信的时候再做具体操作
+    @ApiOperation(value = "发送签名秘钥  发送短信的时候再做具体操作")
+    @PutMapping("sendKey/{id}")
+    public Result lockHospitalSet(@PathVariable Long id) {
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+        String signKey = hospitalSet.getSignKey();
+        String hoscode = hospitalSet.getHoscode();
+        //TODO 发送短信
+        return Result.ok();
+    }
 
 }
